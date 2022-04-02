543e7adc8398d4926d4b185359bf69c0790c0b08
Andrew Li <zeyu.li23@gmail.com>
2022-04-02 16:22:08 -0600

    updated starter script

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 title="$1"
 
-(bundle exec jekyll post "$title" && cd _posts && explorer.exe . && cd .. & bundle exec jekyll serve --port 4001)
+(bundle exec jekyll post "$title" && cd _posts && explorer.exe . && cd .. & bundle exec jekyll serve --port 4001 && xdg-open http://127.0.0.1:4001)
